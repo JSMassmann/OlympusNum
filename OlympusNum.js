a05ce34c92be8d08e1e9dbd67f45442b55fecdd4
solarzone1010 <139612896+solarzone1010@users.noreply.github.com>
2024-02-11 07:14:11 +1300

    Update OlympusNum.js

--- OlympusNum.js
+++ OlympusNum.js
@@ -10,7 +10,6 @@
             // An ordinal is represented as either 0, type 0, or ψ_n(α)*k+β, type 1.
             // n, k are stored as bigints.
             if (v == null) {
-                this.type = 0;
                 this.sub = null;
                 this.arg = null;
                 this.coef = null;
@@ -18,7 +17,7 @@
             }
             else if (v instanceof Ordinal){
                 // Shallow or deep clone all the attributes of v.
-                this.type = v.type;
+                //this.type = v.type;
                 this.sub = v.sub;
                 this.arg = structuredClone(v.arg);
                 this.coef = v.coef;
@@ -26,7 +25,6 @@
             }
             else if (typeof(v) == "bigint") {
                 if (v == 0n) {
-                    this.type = 0;
                     this.sub = null;
                     this.arg = null;
                     this.coef = null;
@@ -34,7 +32,6 @@
                 }
                 else {
                     // 1 = ψ_0(0), and generally n = ψ_0(0)*n.
-                    this.type = 1;
                     this.sub = 0n;
                     this.arg = Ordinal.Ord(0);
                     this.coef = v;
@@ -46,24 +43,38 @@
             }
             else if (typeof(v) == "string") {
                 // Valid strings are either 0, ψ_n(b) or ψ_n(b)+c. We match these via RegEx.
-                const pattern1 = /ψ\_\d+\(.+\)/;
-                const pattern2 = /ψ\_\d+\(.+\)\+.+/;
-                if (v == "0") {
-                    this.type = 0;
-                    this.sub = null;
-                    this.arg = null;
-                    this.coef = null;
-                    this.add = null;
-                }
-                else if (pattern1.test(v)) {
-                    this.type = 1;
-                    this.add = Ordinal.Ord(0);
-                }
-                else if (pattern2.test(v)) {
-                    this.type = 1;
+                if (/\d/.test(v)) {
+                    this = Ordinal.Ord(BigInt(v))
                 }
                 else {
-                    throw new Error("Invalid string to convert.");
+                    let v=v.replaceAll('ω','ψ_0(1)').replaceAll('*','');
+                    let i=v.indexOf('(');
+                    let j=i;
+                    let p=1;
+                    while(1){
+                        j++;
+                        if(v[i]=='('){p++;}
+                        if(v[i]==')'){p--;}
+                        if(!p){break;}
+                    }
+                    let s=BigInt(v.slice(2,i));
+                    let a=v.slice(i+1,j);
+                    let c=1n;
+                    let b='0';
+                    if(j<v.length-1){
+                        if(a[j+1]=='+'){b=a.slice(j+1);}
+                        else{
+                            if(a.slice(j+1).includes('+')){
+                                c=BigInt(a.slice(j+1,a.indexOf('+')));
+                                b=a.slice(a.indexOf('+')+1);
+                            }
+                            else{c=BigInt(a.slice(j+1));}
+                        }
+                    }
+                    this.sub=s;
+                    this.arg=Ordinal.Ord(a);
+                    this.coef=c;
+                    this.add=Ordinal.Ord(b);
                 }
             }
         }
